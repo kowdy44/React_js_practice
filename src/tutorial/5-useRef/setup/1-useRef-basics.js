3f0200bf0bb9e41bfe2e98728180a4577e141e59
Kowdy <kowdy44@gmail.com>
2021-08-04 22:36:30 +0530

    useRef and useReducer

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,25 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer= useRef(null);
+
+  const handleSubmit=function(e){
+    e.preventDefault();
+    console.log(refContainer.current.value);
+
+  }
+  useEffect(()=>{
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  })
+  return <>
+  <form className="form" onSubmit={handleSubmit}>
+    <div>
+    <input type="text" ref={refContainer}></input>
+    <button type="submit" className="btn" >Submit</button>
+    </div>
+  </form>
+  </>;
 };
 
 export default UseRefBasics;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,65 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = function(state,action){
+  if(action.type==="ADD_ITEM"){
+    const newPeople=[...state.people,action.payload]
+    return {...state,people:newPeople,isModelOpen:true,modalContent:"item added"}
+  }
+  if(action.type==="NO_VALUE"){
+    return {...state,isModelOpen:true,modalContent:"Please Enter value"}
+  }
+  if(action.type==="CLOSE_MODAL"){
+    return{...state,isModelOpen:false};
+  }
+  if(action.type==="REMOVE_ITEM"){
+    const newPeople=state.people.filter((person)=>person.id!==action.payload)
+    return{...state,people:newPeople};
+  }
+  throw new Error('No matching action');
+}
+
+const defaultState={
+  people:[],
+  isModelOpen:false,
+  modalContent:""
+}
+
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name,setName]=useState('')
+  // const [people,setPeople]=useState(data);
+  // const [showModal,setShowModal]= useState(false);
+  const [state,dispatch] = useReducer(reducer,defaultState);
+
+  const handleSubmit=function(e){
+    e.preventDefault();
+    if(name){
+      const newItem={id:new Date().getTime().toString(),name}
+      dispatch({type:'ADD_ITEM',payload:newItem})
+      setName('');
+    }else{
+      dispatch({type:"NO_VALUE"});
+    }
+  }
+  const closeModal=function(){
+    dispatch({type:"CLOSE_MODAL"});
+  }
+  return <>
+  {state.isModelOpen && <Modal closeModal={closeModal} modalContent={state.modalContent}></Modal>}
+  <form onSubmit={handleSubmit} className="form">
+    <div>
+    <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}></input>
+    </div>
+    <button type="submit" className="btn">add</button>
+  </form>
+  {state.people.map((person)=>{
+    return<div key={person.id} className="item">
+      <h4>{person.name}</h4>
+      <button onClick={()=>{dispatch({type:"REMOVE_ITEM",payload:person.id})}}>remove</button>
+    </div>
+  })}
+  </>;
 };
 
 export default Index;
